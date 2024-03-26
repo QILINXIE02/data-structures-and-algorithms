959e52a5f359bf9d2c50fda959fd0d5df0cfd728
Qilin Xie <qxie@gicw.org>
2024-03-26 13:45:26 -0700

    challenge-6

--- javascript/code-challenges/challenges-02.test.js
+++ javascript/code-challenges/challenges-02.test.js
@@ -78,8 +78,9 @@ Write a function named mapTwoToThe that produces the same output as your forLoop
 ------------------------------------------------------------------------------------------------ */
 
 const mapTwoToThe = (arr) => {
-  // Solution code here...
+  return arr.map(number => Math.pow(2, number));
 };
+
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 7 - Stretch Goal
