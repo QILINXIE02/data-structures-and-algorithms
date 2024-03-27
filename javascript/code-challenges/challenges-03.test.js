90dcde1b07d9c8e9f9d9ebaa4796c8a26b155831
Qilin Xie <qxie@gicw.org>
2024-03-27 13:04:43 -0700

    challenge3

--- javascript/code-challenges/challenges-03.test.js
+++ javascript/code-challenges/challenges-03.test.js
@@ -35,7 +35,7 @@ For example, containsAnd(['panda', 'ran', 'and']) returns ['panda', 'and'].
 ------------------------------------------------------------------------------------------------ */
 
 const containsAnd = (arr) => {
-  // Solution code here...
+  return arr.filter(item => item.includes('and'));
 };
 
 /* ------------------------------------------------------------------------------------------------
