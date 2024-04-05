a7d0b315d317005b1713571489500fef1d987e46
qilinxie02 <qxie@gicw.org>
2024-04-05 08:47:38 -0700

    challenge3

--- javascript/code-challenges/challenges-10.test.js
+++ javascript/code-challenges/challenges-10.test.js
@@ -52,9 +52,14 @@ For example:
 return: 35
 ------------------------------------------------------------------------------------------------ */
 const totalSum = (matrix) => {
-  // Solution code here...
+  let sum = 0;
+  matrix.forEach(row => {
+    row.forEach(num => {
+      sum += num;
+    });
+  });
+  return sum;
 };
-
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 4
@@ -79,7 +84,6 @@ const alkiBeach = [33, 31, 147, 130, 27, 93, 38, 126, 141, 63, 46, 17];
 const cookieStores = [firstPike, seaTac, seattleCenter, capHill, alkiBeach];
 
 const grandTotal = (stores) => {
-  // Solution code here...
 
 };
 
