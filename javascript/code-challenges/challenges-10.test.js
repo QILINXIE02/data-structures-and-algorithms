2b481907b15261349cfd06ee3ec709765be38894
qilinxie02 <qxie@gicw.org>
2024-04-05 08:48:27 -0700

    challenge4

--- javascript/code-challenges/challenges-10.test.js
+++ javascript/code-challenges/challenges-10.test.js
@@ -84,7 +84,15 @@ const alkiBeach = [33, 31, 147, 130, 27, 93, 38, 126, 141, 63, 46, 17];
 const cookieStores = [firstPike, seaTac, seattleCenter, capHill, alkiBeach];
 
 const grandTotal = (stores) => {
-
+  const hourlySales = [];
+  for (let i = 0; i < hoursOpen.length; i++) {
+    let sum = 0;
+    for (let j = 0; j < stores.length; j++) {
+      sum += stores[j][i];
+    }
+    hourlySales.push(sum);
+  }
+  return hourlySales;
 };
 
 /* ------------------------------------------------------------------------------------------------
